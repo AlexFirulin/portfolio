3cb96aeff0e9fedf1349d1e2dca3eb8a8b4a9d15
AlexFirulin <1993firulin@gmail.com>
2024-01-25 20:05:18 +0200

    fix bugs

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="flex flex-col justify-evenly items-center">
  <div>{{ data.joke }}</div>
- <button @click="fetchRandomJoke()" class="p-2 mt-8 max-w-40 border-gray-400 border">Random joke</button>
 </div>
 </template>
 
 <script setup>
-const { data } = await useAsyncData(() => $fetch('https://geek-jokes.sameerkumar.website/api?format=json')
-)
-const fetchRandomJoke = () => window.location.reload()
+
+const { data } = await useAsyncData(() => $fetch('https://geek-jokes.sameerkumar.website/api?format=json'))
 </script>
